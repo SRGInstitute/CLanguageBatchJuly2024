8c10d0268d1dd695cf316b6f655fb09b91119e99
SRG 04 <khushisingh23372@gmail.com>
2024-09-14 11:23:12 +0530

    my first c-program to github

---
+++ sum.c
@@ -0,0 +1,10 @@
+\\sum of two numbers
+#include<stdio.h>
+int main(){
+int a,b,sum;
+printf("enter two numbers: \n");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("sum of two numbers %d and %d is: %d",a,b,sum);
+return 0;
+}

52b6a8917e33bc5791581e3d6a3b561f290a8859
Arun <kashyaparun292000@gmail.com>
2024-09-14 11:20:17 +0530

    my first java program to github

---
+++ duplicate.java
@@ -0,0 +1,28 @@
+class duplicate 
+{
+    public static void main(String[] args) {
+        int x[] = {10,20,20,30,30,40,50,50,60};
+        int f=0;
+        int n = x.length;
+
+        for(int i=0; i<=n; i++)
+        {
+            f=0;
+            for(int j=i+1; j<=n; j++)
+            {
+                if(x[i]==x[j])
+                {
+                    f=1;
+
+                }
+
+            }
+
+            if(f==1)
+            continue;
+            System.out.println(x[i]);
+
+        }
+
+    }
+}

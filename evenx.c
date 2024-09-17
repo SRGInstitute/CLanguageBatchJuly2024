5f9a6686294b58ce564d8ba87bb56d67e325b183
Kunal Gupta <kkg159487263@gmail.com>
2024-09-17 21:15:51 +0530

    new One

---
+++ evenx.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+int odd,even,x;
+printf("Enter the no of x;");
+scanf("%d",&x);
+for (int i=1; x<=1; i++){
+
+if (i%2==0){
+printf("The no is even\n");
+}
+else{
+printf("The no is odd\n");
+}
+}
+return 0;
+}

93452fd9b5c2f1b6c344ef0484e403828d464749
Arun <kashyaparun292000@gmail.com>
2024-09-29 00:37:33 +0530

    Day 8 arrange negative and positive number

--- 50DaysOfArrayProblem/negativeandpositive.c
+++ 50DaysOfArrayProblem/negativeandpositive.c
@@ -3,10 +3,10 @@
  {
 
 	int i,j,temp,size, nthlargest,k,smallest,p;
-	int arr[6]={4,1,3,6,8,5};
-	//  size=sizeof(arr)/sizeof(arr[0]);
-	printf("Unsorted array is: ");
-	for(i=0;i<6;i++)
+	int arr[]={1, 2, -5, -8, 3, -4, 0, -2, 5, -1, -6, 6, 8, 4, };
+	 size=sizeof(arr)/sizeof(arr[0]);
+	printf("Given array is: ");
+	for(i=0;i<size;i++)
 
 {
 	printf("%d \t",arr[i]);
@@ -14,9 +14,9 @@
 }
 
 
-	for(i=0;i<6;i++)
+	for(i=0;i<size;i++)
 {
-	for(j=0;j<6-i-1;j++)
+	for(j=0;j<size-i-1;j++)
 {
 
 	if(arr[j]>arr[j+1])
@@ -32,7 +32,7 @@
 
 }
 printf("\nSorted array is: ");
-for(i=0;i<6;i++){
+for(i=0;i<size;i++){
 	printf("%d\t",arr[i]);
 }
 }

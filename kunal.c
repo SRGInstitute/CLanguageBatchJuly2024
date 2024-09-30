089c2fe777c9ff5e8c1d7a8f1ce871f1125c7475
brahman-himanshu <hd674552@gmail.com>
2024-09-30 13:50:57 +0530

    add file .c

---
+++ kunal.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+	int number =42;
+	int  knowledge=0;
+	printf("print the kunal  result = \n");
+	if (number<knowledge){
+		printf("number = %d ",number);
+	}
+	else {
+		printf("knowledge = %d ",knowledge);
+	}
+		return 0;
+	}

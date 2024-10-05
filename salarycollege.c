a9c9e24a978755c37079304dd5d52e568c6acd99
brahman-himanshu <hd674552@gmail.com>
2024-10-05 23:16:54 +0530

    add today code

--- salarycollege.c
+++ salarycollege.c
@@ -2,9 +2,9 @@
 int main() {
     float basicSalary, TA , da, grossSalarY;
     printf("Enter the basic salary: ");
-    scanf("%f", &basicSalary);
-    TA = basicSalary*(20 / 100);
-    da = basicSalary*(40 /100 );
+    scanf("%d", &basicSalary);
+    TA = basicSalary*(20.0 / 100);
+    da = basicSalary*(40.0 /100 );
     grossSalarY = basicSalary + TA + da;
     printf("The Gross Value is: %.2f \n", grossSalarY);
     return 0;
